2f7c11dab456dec171e010c86550fc45c99d6df4
FlyingKoala01 <isaac.iglesias.vila19@gmail.com>
2022-03-05 14:45:20 +0100

    Working stars animation

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import './Main.css';
+import './Stars.css';
 
 export default function Main() {
     return (
         <main>
-            <div id='stars'></div>
-            <div id='stars2'></div>
-            <div id='stars3'></div>
+            <div className='animate-flicker' id='stars1'></div>
+            <div className='animate-flicker' id='stars11'></div>
+            <div className='animate-flicker' id='stars111'></div>
+            <div className='animate-flicker' id='stars2'></div>
+            <div className='animate-flicker' id='stars22'></div>
+            <div className='animate-flicker' id='stars222'></div>
+            <div className='animate-flicker' id='stars3'></div>
+            <div className='animate-flicker' id='stars33'></div>
+            <div className='animate-flicker' id='stars333'></div>
+            
+            <h1 className='myName'>Isaac Iglesias Vila</h1>
         </main>
     )
 }
