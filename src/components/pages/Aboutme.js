b07104ffc0f62a8b8743eaf9aa25170a9020feda
FlyingKoala01 <flyingkoala01@gmail.com>
2022-03-07 10:37:06 +0100

    display text onclick Aboutme

--- src/components/pages/Aboutme.js
+++ src/components/pages/Aboutme.js
@@ -1,36 +1,18 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react'
+import { Button } from '../Button.js';
 import '../../App.css';
 
 export default function Aboutme() {
+    this.setState = {
+        showMessage: false
+    }
+
     return (
         <main>
-            <h2 className='trial'>Who am I?</h2>
-            <h2 className='trial'>Who am I?</h2>
-            <h2 className='trial'>Who am I?</h2>
-            <h2 className='trial'>Who am I?</h2>
-            <h2 className='trial'>Who am I?</h2>
-            <h2 className='trial'>Who am I?</h2>
-            <h2 className='trial'>Who am I?</h2>
-            <h2 className='trial'>Who am I?</h2>
-            <h2 className='trial'>Who am I?</h2>
-            <h2 className='trial'>Who am I?</h2>
-            <h2 className='trial'>Who am I?</h2>
-            <h2 className='trial'>Who am I?</h2>
-            <h2 className='trial'>Who am I?</h2>
-            <h2 className='trial'>Who am I?</h2>
-            <h2 className='trial'>Who am I?</h2>
-            <h2 className='trial'>Who am I?</h2>
-            <h2 className='trial'>Who am I?</h2>
-            <h2 className='trial'>Who am I?</h2>
-            <h2 className='trial'>Who am I?</h2>
-            <h2 className='trial'>Who am I?</h2>
-            <h2 className='trial'>Who am I?</h2>
-            <h2 className='trial'>Who am I?</h2>
-            <h2 className='trial'>Who am I?</h2>
-            <h2 className='trial'>Who am I?</h2>
-            <h2 className='trial'>Who am I?</h2>
-            
-            <p >An aspiring ICT engineer, interested in the many diverse fields concerning technology.</p>
+            <div className='childhood'>
+                {this.state.showMessage && <p>Hi</p>}
+                <Button onClick={() => {this.setState({showMessage: true})}}>childhood</Button> 
+            </div>
         </main>
     )
 }
