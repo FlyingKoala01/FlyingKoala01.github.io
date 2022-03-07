c30c8187cb5a85bbae94ee65eb483b01ae5f8080
FlyingKoala01 <isaac.iglesias.vila19@gmail.com>
2022-03-07 19:14:53 +0100

    loader added and buttons in about me working

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { BrowserRouter as Router, Routes , Route } from 'react-router-dom';
+import RingLoader from "react-spinners/RingLoader";
 
 import Main from "./components/pages/Main"
 import Navbar from "./components/Navbar"
@@ -10,12 +11,21 @@ import ContactMe from './components/pages/ContactMe';
 import './App.css';
 
 
-
 export default function App() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 3000)
+  }, [])
   return (
-    <>
+    <div className='App'>
+      
+      {loading ? (<div className='Loader'><RingLoader size = {150} color = {"#FFFFFF"} loading = {loading}/> </div>
+      ) : (
       <Router>
-        <Navbar />
         <Routes>
             <Route path='/' element={<Main />}/>
             <Route path='/AboutMe' element={<Aboutme />}/>
@@ -23,9 +33,10 @@ export default function App() {
             <Route path='/InProgress' element={<WorkInProgress />}/>
             <Route path='/ContactMe' element={<ContactMe />}/>
         </Routes>
-        
+        <Navbar />
       </Router>
-    </>
+      )}
+    </div>
   )
 }
 
--- src/components/pages/Aboutme.js
+++ src/components/pages/Aboutme.js
@@ -1,17 +1,46 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState} from 'react'
 import { Button } from '../Button.js';
 import '../../App.css';
 
 export default function Aboutme() {
-    this.setState = {
-        showMessage: false
-    }
+    let [showHideChildHood, setShowHideChildHood] = useState(false);
+    let [showHideTravelling, setShowHideTravelling] = useState(false);
+    let [showHideUniversity, setShowHideUniversity] = useState(false);
+    let [showHideFuture, setShowHideFuture] = useState(false);
 
     return (
         <main>
             <div className='childhood'>
-                {this.state.showMessage && <p>Hi</p>}
-                <Button onClick={() => {this.setState({showMessage: true})}}>childhood</Button> 
+                {showHideChildHood && <p className='trial'>Childhood Section</p>}
+                <Button onClick={() => {setShowHideChildHood(!showHideChildHood); 
+                    setShowHideTravelling(showHideTravelling = false); 
+                    setShowHideUniversity(showHideUniversity = false);
+                    setShowHideFuture(showHideFuture = false)
+                    }}>Childhood</Button> 
+            </div>
+            <div className='Travelling'>
+                {showHideTravelling && <p className='trial'>Travelling Section</p>}
+                <Button onClick={() => {setShowHideChildHood(showHideChildHood = false); 
+                    setShowHideTravelling(!showHideTravelling); 
+                    setShowHideUniversity(showHideUniversity = false);
+                    setShowHideFuture(showHideFuture = false)
+                    }}>Travelling</Button> 
+            </div>
+            <div className='University'>
+                {showHideUniversity && <p className='trial'>University Section</p>}
+                <Button onClick={() => {setShowHideChildHood(showHideChildHood = false); 
+                    setShowHideTravelling(showHideTravelling = false);
+                    setShowHideUniversity(!showHideUniversity);
+                    setShowHideFuture(showHideFuture = false)
+                    }}>University</Button> 
+            </div>
+            <div className='Future'>
+                {showHideFuture && <p className='trial'>Future Section</p>}
+                <Button onClick={() => {setShowHideChildHood(showHideChildHood = false); 
+                    setShowHideTravelling(showHideTravelling = false);
+                    setShowHideUniversity(showHideUniversity = false);
+                    setShowHideFuture(!showHideFuture) 
+                    }}>Future</Button> 
             </div>
         </main>
     )
