11ff718ed7788919238208ca5307ee085e0eef7f
FlyingKoala01 <flyingkoala01@gmail.com>
2022-03-12 21:53:11 +0100

    Started writing the contact me form

--- src/components/pages/ContactMe.js
+++ src/components/pages/ContactMe.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react'
 
 import SocialFollow from "../SocialFollow"
+import { Button } from '../Button.js';
 
 import '../../App.css';
 
 
 export default function ContactMe() {
     
+    const [sender_name, set_sender_name] = setState('');
+    const [sender_email, set_sender_email] = setState('');
+    const [message, set_message] = setState('');
     return (
         <div>
             <SocialFollow />
+            <h1>Contact Me</h1>
+            <form>
+                <input type = "text" name = "sender_name" value = {sender_name} required_placeholder = "Your Name"/>
+                <input type="email" name = "sender_email" value = {sender_email} required_placeholder = "Your Email" />
+                <textarea type="message" value = {message} required_placeholder = "Your Message" />
+                <Button>Send Email</Button>
+            </form>
         </div>
     )
 }
