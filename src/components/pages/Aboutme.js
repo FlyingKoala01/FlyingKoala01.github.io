638665e95de3ac65068250e501b4e8520b8890b4
FlyingKoala01 <isaac.iglesias.vila19@gmail.com>
2022-03-23 17:57:08 +0100

    Updated ToDo and about me section

--- src/components/pages/Aboutme.js
+++ src/components/pages/Aboutme.js
@@ -26,12 +26,19 @@ export default function Aboutme() {
     else {
         return (
             <main className='aboutMeBackground'>
-                <div className='glassmorphism'>
-                    <p className='textAboutMe'>Born in Milan, Italy in the year 2000. Raised in a bilingual family, spending most of my childhood travelling in between countries, Specifically in Italy, Spain, Russia and Mexico. </p>
-                    <p className='textAboutMe'>As a result, I've learnt 4 languages at a proficient level and increased my ability to adapt to change.</p>
-                    <p className='textAboutMe'>I am currently studying an <a href='https://www.upc.edu/en/bachelors/ict-systems-engineering-manresa-epsem' alt="UPC EPSEM">ICT Engineering degree</a> in Spain. And, although I am deeply interested in the whole technology spectrum, I intend to persue a carreer in either web development or cybersecurity.</p>
+                <h1 className='titlePage' style={{fontSize: "55px", left:"5%", letterSpacing: "0.199cm" }}>what should you know about me</h1>
+                <div className='personDiv'>
+                    <h2 className='subtitlePage'>as a person</h2>
+                    <p className='textAboutMe'>Raised in a <b>bilingual</b> family, spending most of my childhood travelling in between countries, Specifically in Italy, Spain, Russia and Mexico. As a result, I've learnt <b>4</b> languages at a <b>proficient</b> level and increased my ability to <b>adapt</b> to change.</p>
+                    <p className='textAboutMe'>I consider myself a very dynamic person, able to tackle the fuyck donfBFUIA</p>
                     <p className='textAboutMe'></p>
                     <p className='textAboutMe'></p>
+                    <p className='textAboutMe'></p>
+                </div>
+                <div className='engineerDiv'>
+                    <h2 className='subtitlePage'>as an engineer</h2>
+                    <p className='textAboutMe'>I am currently studying an <a classname="degref" href='https://www.upc.edu/en/bachelors/ict-systems-engineering-manresa-epsem' alt="UPC EPSEM">ICT Engineering degree</a> in Spain. And, although I am deeply interested in the whole technology spectrum, I intend to persue a carreer in either <b>web development</b> or <b>cybersecurity</b>.</p>
+                    <p className='textAboutMe'>Throughout these years, I had been introduced to the following programming languages:</p>
                 </div>
                 <div>
                     <img src={'../../Images/me.png'} className='myFace' alt="" />
