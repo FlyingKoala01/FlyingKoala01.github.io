7c79a0d497802955645fbae68cbc4c76475194ae
FlyingKoala01 <isaac.iglesias.vila19@gmail.com>
2022-03-24 19:07:48 +0100

    Worked on about me

--- src/components/pages/Aboutme.js
+++ src/components/pages/Aboutme.js
@@ -29,8 +29,8 @@ export default function Aboutme() {
                 <h1 className='titlePage' style={{fontSize: "55px", left:"5%", letterSpacing: "0.199cm" }}>what should you know about me</h1>
                 <div className='personDiv'>
                     <h2 className='subtitlePage'>as a person</h2>
-                    <p className='textAboutMe'>Raised in a <b>bilingual</b> family, spending most of my childhood travelling in between countries, Specifically in Italy, Spain, Russia and Mexico. As a result, I've learnt <b>4</b> languages at a <b>proficient</b> level and increased my ability to <b>adapt</b> to change.</p>
-                    <p className='textAboutMe'>I consider myself a very dynamic person, able to tackle the fuyck donfBFUIA</p>
+                    <p className='textAboutMe'>Raised in a <b>bilingual</b> family, spending most of my childhood travelling in between countries, Specifically in Italy, Spain, Russia and Mexico. As a result, I've learnt <b>4</b> languages at a <b>proficient</b> level (English, Italian, Spanish and Catalan)</p>
+                    <p className='textAboutMe'> Nevertheless, not only I learnt languages by my expatriate experience. I sincerely believe it has taught me the ability to <b>adapt</b> to change and to work in any workspace and culture.</p>
                     <p className='textAboutMe'></p>
                     <p className='textAboutMe'></p>
                     <p className='textAboutMe'></p>
@@ -38,7 +38,26 @@ export default function Aboutme() {
                 <div className='engineerDiv'>
                     <h2 className='subtitlePage'>as an engineer</h2>
                     <p className='textAboutMe'>I am currently studying an <a classname="degref" href='https://www.upc.edu/en/bachelors/ict-systems-engineering-manresa-epsem' alt="UPC EPSEM">ICT Engineering degree</a> in Spain. And, although I am deeply interested in the whole technology spectrum, I intend to persue a carreer in either <b>web development</b> or <b>cybersecurity</b>.</p>
-                    <p className='textAboutMe'>Throughout these years, I had been introduced to the following programming languages:</p>
+                    <p className='textAboutMe'>Throughout these years, I had been studying the following programming languages:</p>
+                    <div class="wrapper">
+                        <div class="one">Python</div>
+                        <div class="two">Go</div>
+                        <div class="three">C</div>
+                        <div class="four">Assembly</div>
+                        <div class="five">Javascript</div>
+                        <div class="six">HTML</div>
+                        <div class="seven">CSS</div>
+                        <div class="eight">React</div>
+                    </div>
+                    <p className='textAboutMe'>Additionally, I had been introduced to several technologies, software and other tools:</p>
+                    <div class="wrapper">
+                        <div class="one">Matlab/Octave</div>
+                        <div class="two">NI Multisim</div>
+                        <div class="three">Intel Quartus (ModelSim)</div>
+                        <div class="four">LaTex</div>
+                        <div class="five">Arduino</div>
+                        <div class="six">Git/SVN</div>
+                    </div>
                 </div>
                 <div>
                     <img src={'../../Images/me.png'} className='myFace' alt="" />
