2e8d11f67685dc0e6b76a429a80a675e1cc195b8
FlyingKoala01 <flyingkoala01@gmail.com>
2022-03-25 12:01:30 +0100

    Cleaned up files

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Main from "./components/pages/Main"
-import Banner from "./components/Banner"
 import Navbar from "./components/Navbar"
 import Aboutme from './components/pages/Aboutme';
 import Portfolio from './components/pages/Portfolio';
@@ -21,7 +20,6 @@ export default function App() {
   return (
     <div className='App'>
       <Router>
-        <Banner />
         <Routes>
           <Route path='/' element={<Main />} />
           <Route path='/AboutMe' element={<Aboutme />} />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,40 +1,41 @@
 import React, { useEffect, useState } from "react";
-import { Link, useLocation  } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 import './Navbar.css';
 
 export default function Navbar() {
 
-    const [navLinks, setNavLinks] = useState([]);
+  const [navLinks, setNavLinks] = useState([]);
 
-      useEffect(() => {
-        const navs = [
-          { name: "home", path: "/" },
-          { name: "about", path: "/AboutMe" },
-          { name:"portfolio", path: "/Portfolio"},
-          { name:"contact", path:"/ContactMe"}
-        ];
-        setNavLinks(navs);
-      }, []);
-    
-      const usePathname = () => {
-        const location = useLocation();
-        return location.pathname;
-      }
+  useEffect(() => {
+    const navs = [
+      { name: "home", path: "/" },
+      { name: "about", path: "/AboutMe" },
+      { name: "portfolio", path: "/Portfolio" },
+      { name: "contact", path: "/ContactMe" }
+    ];
+    setNavLinks(navs);
+  }, []);
 
-    return (
-        <>
-        <nav className='navbar'>
-            <ul className={'nav-menu'}>
-              {navLinks.map((d, i) => (
-                <li key={i}className='nav-item'>
-                  <Link to={d.path} className='nav-links' style={ ( usePathname() === `${d.path}` ) ? {borderBottom: '4px solid #FFF' } : {borderBottom: '4px solid #000' } }>
-                      {d.name}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-        </nav>
+  const usePathname = () => {
+    const location = useLocation();
+    return location.pathname;
+  }
+
+  return (
+    <>
+      <nav className='bannerTop'></nav>
+      <nav className='navbar'>
+        <ul className={'nav-menu'}>
+          {navLinks.map((d, i) => (
+            <li key={i} className='nav-item'>
+              <Link to={d.path} className='nav-links' style={(usePathname() === `${d.path}`) ? { borderBottom: '4px solid #FFF' } : { borderBottom: '4px solid #000' }}>
+                {d.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </>
-    )
+  )
 }
