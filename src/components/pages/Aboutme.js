96d4eae8ea7a2cb7e43c450d12df36fffbcda909
FlyingKoala01 <isaac.iglesias.vila19@gmail.com>
2022-03-30 13:36:24 +0200

    Working on icons

--- src/components/pages/Aboutme.js
+++ src/components/pages/Aboutme.js
@@ -26,7 +26,9 @@ export default function Aboutme() {
     const showLogo = (event, key) => {
         event.preventDefault();
         setVisibleLogo({ ...visibleLogo, ...{ [key]: true } });
+
     };
+
 
     //hide logo by given key
     const hideLogo = (event, key) => {
@@ -69,67 +71,64 @@ export default function Aboutme() {
                     <p className='textAboutMe'>I am currently studying an <a classname="degref" href='https://www.upc.edu/en/bachelors/ict-systems-engineering-manresa-epsem' alt="UPC EPSEM">ICT Engineering degree</a> in Spain. And, although I am deeply interested in the whole technology spectrum, I intend to persue a carreer in either <b>web development</b> or <b>cybersecurity</b>.</p>
                     <p className='textAboutMe'>Throughout these years, I had been studying the following programming languages:</p>
                     <div class="wrapper">
-                        <div class="one" onMouseOver={(e) => {showLogo(e, 'pyLogo');}}onMouseOut={(e) => {hideLogo(e, 'pyLogo');}}>
-                            {!visibleLogo['pyLogo'] && <p>Python</p>}
-                            {visibleLogo['pyLogo'] && <i class="devicon-python-plain" style={{fontSize:'32px'}}/>}
+                        <div class="one" onMouseOver={(e) => {showLogo(e, 'pyLogo');}} onMouseOut={(e) => {hideLogo(e, 'pyLogo');}}>
+                            {!visibleLogo['pyLogo'] && <p className='textGrid'>Python</p>}
+                            {visibleLogo['pyLogo'] && <i class="devicon-python-plain" style={{fontSize:'48px'}}/>}
                         </div>
-                        <div class="two" onMouseOver={(e) => {showLogo(e, 'goLogo');}}onMouseOut={(e) => {hideLogo(e, 'goLogo');}}>
-                            {!visibleLogo['goLogo'] && <p>Go</p>}
-                            {visibleLogo['goLogo'] && <i class="devicon-go-original-wordmark colored" style={{fontSize:'32px'}} />}
+                        <div class="two" onMouseOver={(e) => {showLogo(e, 'goLogo');}} onMouseOut={(e) => {hideLogo(e, 'goLogo');}}>
+                            {!visibleLogo['goLogo'] && <p className='textGrid'>Go</p>}
+                            {visibleLogo['goLogo'] && <i class="devicon-go-original-wordmark colored" style={{fontSize:'48px'}} />}
                         </div>
-                        <div class="three" onMouseOver={(e) => {showLogo(e, 'cLogo');}}onMouseOut={(e) => {hideLogo(e, 'cLogo');}}>
-                            {!visibleLogo['cLogo'] && <p>C</p>}
-                            {visibleLogo['cLogo'] && <i class="devicon-c-line" style={{fontSize:'32px'}} />}
+                        <div class="three" onMouseOver={(e) => {showLogo(e, 'cLogo');}} onMouseOut={(e) => {hideLogo(e, 'cLogo');}}>
+                            {!visibleLogo['cLogo'] && <p className='textGrid'>C</p>}
+                            {visibleLogo['cLogo'] && <i class="devicon-c-line" style={{fontSize:'48px'}} />}
                         </div>
-                        <div class="four" onMouseOver={(e) => {showLogo(e, 'assemblyLogo');}}onMouseOut={(e) => {hideLogo(e, 'assemblyLogo');}}>
-                            {!visibleLogo['assemblyLogo'] && <p>Assembly</p>}
-                            {visibleLogo['assemblyLogo'] && <i class="devicon-aarch64-plain" style={{fontSize:'32px'}} />}
+                        <div class="four" onMouseOver={(e) => {showLogo(e, 'assemblyLogo');}} onMouseOut={(e) => {hideLogo(e, 'assemblyLogo');}}>
+                            {!visibleLogo['assemblyLogo'] && <p className='textGrid'>Assembly</p>}
+                            {visibleLogo['assemblyLogo'] && <i class="devicon-aarch64-plain" style={{fontSize:'48px'}} />}
                         </div>
-                        <div class="five" onMouseOver={(e) => {showLogo(e, 'jsLogo');}}onMouseOut={(e) => {hideLogo(e, 'jsLogo');}}>
-                            {!visibleLogo['jsLogo'] && <p>Javascript</p>}
-                            {visibleLogo['jsLogo'] && <i class="devicon-javascript-plain colored" style={{fontSize:'32px'}} />}
+                        <div class="five" onMouseOver={(e) => {showLogo(e, 'jsLogo');}} onMouseOut={(e) => {hideLogo(e, 'jsLogo');}}>
+                            {!visibleLogo['jsLogo'] && <p className='textGrid'>Javascript</p>}
+                            {visibleLogo['jsLogo'] && <i class="devicon-javascript-plain colored" style={{fontSize:'48px'}} />}
                         </div>
-                        <div class="six" onMouseOver={(e) => {showLogo(e, 'htmlLogo');}}onMouseOut={(e) => {hideLogo(e, 'htmlLogo');}}>
-                            {!visibleLogo['htmlLogo'] && <p>HTML</p>}
-                            {visibleLogo['htmlLogo'] && <i class="devicon-html5-plain colored" style={{fontSize:'32px'}} />}
+                        <div class="six" onMouseOver={(e) => {showLogo(e, 'htmlLogo');}} onMouseOut={(e) => {hideLogo(e, 'htmlLogo');}}>
+                            {!visibleLogo['htmlLogo'] && <p className='textGrid'>HTML</p>}
+                            {visibleLogo['htmlLogo'] && <i class="devicon-html5-plain colored" style={{fontSize:'48px'}} />}
                         </div>
-                        <div class="seven" onMouseOver={(e) => {showLogo(e, 'cssLogo');}}onMouseOut={(e) => {hideLogo(e, 'cssLogo');}}>
-                            {!visibleLogo['cssLogo'] && <p>CSS</p>}
-                            {visibleLogo['cssLogo'] && <i class="devicon-css3-plain colored" style={{fontSize:'32px'}} />}
+                        <div class="seven" onMouseOver={(e) => {showLogo(e, 'cssLogo');}} onMouseOut={(e) => {hideLogo(e, 'cssLogo');}}>
+                            {!visibleLogo['cssLogo'] && <p className='textGrid'>CSS</p>}
+                            {visibleLogo['cssLogo'] && <i class="devicon-css3-plain colored" style={{fontSize:'48px'}} />}
                         </div>
-                        <div class="eight" onMouseOver={(e) => {showLogo(e, 'reactLogo');}}onMouseOut={(e) => {hideLogo(e, 'reactLogo');}}>
-                            {!visibleLogo['reactLogo'] && <p>React JS</p>}
-                            {visibleLogo['reactLogo'] && <i class="devicon-react-original colored" style={{fontSize:'32px'}} />}
+                        <div class="eight" onMouseOver={(e) => {showLogo(e, 'reactLogo');}} onMouseOut={(e) => {hideLogo(e, 'reactLogo');}}>
+                            {!visibleLogo['reactLogo'] && <p className='textGrid'>React JS</p>}
+                            {visibleLogo['reactLogo'] && <i class="devicon-react-original colored" style={{fontSize:'48px'}} />}
                         </div>
                     </div>
                     <p className='textAboutMe'>Additionally, I had been introduced to several technologies, software and other tools:</p>
                     <div class="wrapper">
                         <div class="one" onMouseOver={(e) => {showLogo(e, 'matlabLogo');}}onMouseOut={(e) => {hideLogo(e, 'matlabLogo');}}>
                             {!visibleLogo['matlabLogo'] && <p>Matlab/Octave</p>}
-                            {visibleLogo['matlabLogo'] && <i class="devicon-matlab-plain" style={{fontSize:'32px'}}/>}
+                            {visibleLogo['matlabLogo'] && <i class="devicon-matlab-plain" style={{fontSize:'48px'}}/>}
                         </div>
                         <div class="two">NI Multisim</div>
                         <div class="three">Intel Quartus (ModelSim)</div>
                         <div class="four" onMouseOver={(e) => {showLogo(e, 'latexLogo');}}onMouseOut={(e) => {hideLogo(e, 'latexLogo');}}>
                             {!visibleLogo['latexLogo'] && <p>LaTeX</p>}
-                            {visibleLogo['latexLogo'] && <i class="devicon-latex-original" style={{fontSize:'32px'}}/>}
+                            {visibleLogo['latexLogo'] && <i class="devicon-latex-original" style={{fontSize:'48px'}}/>}
                         </div>
                         <div class="five" onMouseOver={(e) => {showLogo(e, 'arduinoLogo');}}onMouseOut={(e) => {hideLogo(e, 'arduinoLogo');}}>
                             {!visibleLogo['arduinoLogo'] && <p>Arduino</p>}
-                            {visibleLogo['arduinoLogo'] && <i class="devicon-arduino-plain" style={{fontSize:'32px'}}/>}
+                            {visibleLogo['arduinoLogo'] && <i class="devicon-arduino-plain" style={{fontSize:'48px'}}/>}
                         </div>
                         <div class="six" onMouseOver={(e) => {showLogo(e, 'gitLogo');}}onMouseOut={(e) => {hideLogo(e, 'gitLogo');}}>
                             {!visibleLogo['gitLogo'] && <p>Git/SVN</p>}
-                            {visibleLogo['gitLogo'] && <i class="devicon-git-plain colored" style={{fontSize:'32px'}}/>}
+                            {visibleLogo['gitLogo'] && <i class="devicon-git-plain colored" style={{fontSize:'48px'}}/>}
                         </div>
                         <div class="seven" onMouseOver={(e) => {showLogo(e, 'linuxLogo');}}onMouseOut={(e) => {hideLogo(e, 'linuxLogo');}}>
                             {!visibleLogo['linuxLogo'] && <p>Linux</p>}
-                            {visibleLogo['linuxLogo'] && <i class="devicon-linux-plain" style={{fontSize:'32px'}}/>}
+                            {visibleLogo['linuxLogo'] && <i class="devicon-linux-plain" style={{fontSize:'48px'}}/>}
                         </div>
                     </div>
-                </div>
-                <div>
-                    <img src={'../../Images/me.png'} className='myFace' alt="" />
                 </div>
             </main>
         )
