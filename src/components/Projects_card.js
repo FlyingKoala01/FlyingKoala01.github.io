e77738da6cbdda519fd2338989588b1edc00f4e7
FlyingKoala01 <isaac.iglesias.vila19@gmail.com>
2022-04-02 14:58:49 +0200

    Started working on portfolio

--- src/components/Projects_card.js
+++ src/components/Projects_card.js
@@ -1,25 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function CardItem(props) {
+export default function CardItem(props) {
   return (
     <>
-      <li className='cards__item'>
-        <Link className='cards__item__link' to={props.path}>
-          <figure className='cards__item__pic-wrap' data-category={props.label}>
+      <div className={props.position}>
+        <Link className='cardsItemLink' to={props.path}>
+          <figure className='cardsItemPicWrap' data-category={props.label}>
             <img
-              className='cards__item__img'
+              className='cardsItemImg'
               alt={props.label}
               src={props.src}
             />
           </figure>
-          <div className='cards__item__info'>
-            <h5 className='cards__item__text'>{props.text}</h5>
+          <div className='cardsItemInfo'>
+            <p className='cardsItemText'>{props.text}</p>
           </div>
         </Link>
-      </li>
+      </div>
     </>
   );
 }
-
-export default CardItem;
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,64 +1,54 @@
 import React, { useState, useEffect } from 'react'
 
-import '../Cards.css';
 import CardItem from '../Projects_card';
 import Loader from '../Loader'
 
+import '../../App.css';
+
 export default function Projects() {
 
-  // Set loading state to true initially
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    // Loading function to load data or 
-    // fake it using setTimeout;
+
     const loadData = async () => {
 
-      // Wait for two second
       await new Promise((r) => setTimeout(r, 3000));
 
-      // Toggle loading state
       setLoading((loading) => !loading);
     };
 
     loadData();
   }, [])
 
-  // If page is in loading state, display 
-  // loading message. Modify it as per your 
-  // requirement.
   if (loading) {
     return <Loader value={"Take a look at what I've been working on!"} />
   }
   else {
     return (
-      <div className='cards'>
-        <h1>Portfolio</h1>
-        <div className='cards__container'>
-          <div className='cards__wrapper'>
-            <ul className='cards__items'>
-              <CardItem
-                src={require('../../Images/React.png')}
-                text='Learnt React.JS and Node.JS by developing my personal website. The objective is, in a future, develop another website and learn the MERN stack.'
-                label='React Portfolio'
-                path='/'
-              />
-              <CardItem
-                src={require('../../Images/Python.png')}
-                text='Simple project to practice Python'
-                label='Python Tetris'
-                path='/'
-              />
-            </ul>
-            <ul className='cards__items'>
-              <CardItem
-                src={require('../../Images/React.png')}
-                text='Studying for Security+'
-                label='Cybersecurity'
-                path='/'
-              />
-            </ul>
-          </div>
+      <div className='portfolioSection'>
+        <div className='cardsContainer'>
+            <CardItem
+              position='itemOne'
+              src={require('../../Images/React.png')}
+              text='Learnt React.JS and Node.JS by developing my personal website. The objective is, in a future, develop another website and learn the MERN stack.'
+              label='React Portfolio'
+              path='/'
+            />
+            <CardItem
+              position='itemSecond'
+              src={require('../../Images/React.png')}
+              text='Simple project to practice Python'
+              label='Python Tetris'
+              path='/'
+            />
+            <CardItem
+              position='itemThree'
+              src={require('../../Images/React.png')}
+              text='Studying for Security+'
+              label='Cybersecurity'
+              path='/'
+            />
         </div>
       </div>
     );
