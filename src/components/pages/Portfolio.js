de5a72f019bd17e246d23bee9af707f83d5f4b1b
FlyingKoala01 <isaac.iglesias.vila19@gmail.com>
2022-04-03 22:52:00 +0200

    Overall idea for portfolio done

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,11 +1,32 @@
 import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom';
 
-import CardItem from '../Projects_card';
 import Loader from '../Loader'
 
 import '../../App.css';
 
 export default function Projects() {
+
+  const itemTitle = {
+    itemOneTitle: false,
+    itemTwoTitle: false,
+  };
+
+  const [visibleTitle, setVisibleTitle] = useState(itemTitle);
+
+  const showTitle = (event, key) => {
+    event.preventDefault();
+    console.log("IN");
+    setVisibleTitle({ ...visibleTitle, ...{ [key]: true } });
+    console.log(itemTitle);
+  };
+
+  const hideTitle = (event, key) => {
+    event.preventDefault();
+    console.log("OUT");
+    setVisibleTitle({ ...visibleTitle, ...{ [key]: false } });
+    console.log(itemTitle);
+  };
 
   const [loading, setLoading] = useState(true);
 
@@ -28,35 +49,47 @@ export default function Projects() {
     return (
       <div className='portfolioSection'>
         <div className='cardsContainer'>
-          <CardItem
-            position='itemOne'
-            src={require('../../Images/reactProject.PNG')}
-            text='Learnt React.JS by developing my personal website. The objective is, in a future, develop another website and learn the MERN stack.'
-            label='React Portfolio'
-            path='/'
-            Title='REACT'
-          />
-          <CardItem
-            position='itemSecond'
-            src={require('../../Images/Python.png')}
-            text='Incoming...'
-            label='Future'
-            path='/'
-          />
-          <CardItem
-            position='itemThird'
-            src={require('../../Images/Python.png')}
-            text='Incoming...'
-            label='Future'
-            path='/'
-          />
+          <Link to='/'className="cardLinks">
+            <div className='itemOne' onMouseOver={(e) => {showTitle(e, 'itemOneTitle');}} onMouseOut={(e) => {hideTitle(e, 'itemOneTitle');}}>
+                <img
+                  className='cardsItemImg'
+                  alt='React Portfolio'
+                  src={require('../../Images/reactProject.PNG')}
+                />
+                <div className='cardsItemInfo'>
+                  <p className='cardsItemText'>Learnt React.JS by developing my personal website. The objective is, in a future, develop another website and learn the MERN stack.</p>
+                </div>
+            </div>
+          </Link>
+          <Link to='/'className="cardLinks">
+            <div className='itemSecond' onMouseEnter={e => {showTitle(e, 'itemTwoTitle')}} onMouseLeave={e => {hideTitle(e, 'itemTwoTitle')}}>
+                <img
+                  className='cardsItemImg'
+                  alt='Future'
+                  src={require('../../Images/KaliLogo.png')}
+                />
+                <div className='cardsItemInfo'>
+                  <p className='cardsItemText'>Incoming</p>
+                </div>
+            </div>
+          </Link>
         </div>
-        <div className='cardTitle'>
-          <p className='cardTitleText'>REACT</p>
-        </div>
-        <div className='cardDate'>
-          <p className='cardDateText'></p>
-        </div>
+        {visibleTitle['itemOneTitle'] &&
+        <div className='cardTitle' style={{top:'0%', animation: 'moveright 1s ease forwards'}}>
+          <p className='cardTitleText'>REACT<br></br>WEBSITE</p>
+        </div>}
+        {visibleTitle['itemOneTitle'] &&
+        <div className='cardDate'style={{top:'0%', animation: 'moveleft 1.5s ease forwards 0.5s'}}>
+          <p className='cardDateText'>DEPLOYED<br></br>04.22</p>
+        </div>}
+        {visibleTitle['itemTwoTitle'] &&
+        <div className='cardTitle' style={{top:'30%', left: '40%', animation: 'moveleft 1.5s ease forwards 0.5s'}}>
+          <p className='cardTitleText'>FUTURE</p>
+        </div>}
+        {visibleTitle['itemTwoTitle'] &&
+        <div className='cardDate' style={{top:'30%', left: '30%', animation: 'moveright 1s ease forwards'}}>
+          <p className='cardDateText'>.22</p>
+        </div>}
       </div>
     );
   }
