b01cd45433fad007437b90eedbf7f08dc809f96c
FlyingKoala01 <isaac.iglesias.vila19@gmail.com>
2022-04-04 15:29:05 +0200

    scroll almost completed

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -3,27 +3,41 @@ import React, { useState, useEffect } from 'react'
 import Loader from '../Loader';
 
 import '../../App.css';
-
+import {HiOutlineArrowCircleUp, HiOutlineArrowCircleDown} from "react-icons/hi";
 export default function Projects() {
 
   const projectsInfo = {
     0: {
       itemTitle: 'itemZeroTitle',
-      cardTitleText: 'ZERO',
-      cardDateText: 'NEVER',
-      cardhref: 'https://github.com/FlyingKoala01/Website'
+      cardTitleText: '',
+      cardDateText: '',
+      cardhref: '',
+      cardSRC:'../../Images/emptybackground.png',
+      cardALT:"EMPTY"
     },
     1: {
       itemTitle: 'itemOneTitle',
       cardTitleText: 'REACT',
       cardDateText: '04.22',
-      cardhref: 'https://github.com/FlyingKoala01/Website'
+      cardhref: 'https://github.com/FlyingKoala01/Website',
+      cardSRC:'../../Images/reactProject.PNG',
+      cardALT:"REACT"
     },
     2: {
       itemTitle: 'itemTwoTitle',
       cardTitleText: 'FUTURE',
       cardDateText: '.22',
-      cardhref: 'https://github.com/FlyingKoala01/Website'
+      cardhref: 'https://github.com/FlyingKoala01/',
+      cardSRC:'../../Images/KaliLogo.png',
+      cardALT:"KALI"
+    },
+    3: {
+      itemTitle: 'itemZeroTitle',
+      cardTitleText: '',
+      cardDateText: '',
+      cardhref: '',
+      cardSRC:'../../Images/emptybackground.png',
+      cardALT:"EMPTY"
     },
   }
 
@@ -69,47 +83,43 @@ export default function Projects() {
     return (
       <div className='portfolioSection'>
         <div className='prevArea' onClick={() => setIndex(index - 1)}>
-          <p className='prevText'>PREV</p>
+          {(index != 0) && <i className='prevText'><HiOutlineArrowCircleUp/></i>}
         </div>
         <div className='cardsContainer'>
           <div className='itemOne'>
-            <a href={projectsInfo[index].cardhref} className="cardLinks">
-              <img
-                className='cardsItemImg'
-                alt='itemOne'
-                src={require('../../Images/KaliLogo.png')}
-              />
-            </a>
+            <img
+              className='cardsItemImg'
+              alt='itemOne'
+              src={require("" + projectsInfo[index].cardSRC)}
+            />
           </div>
           <div className='itemSecond' onMouseOver={(e) => { showTitle(e, projectsInfo[index + 1].itemTitle); }} onMouseOut={(e) => { hideTitle(e, projectsInfo[index + 1].itemTitle); }}>
             <a href={projectsInfo[index + 1].cardhref} className="cardLinks">
               <img
                 className='cardsItemImg'
                 alt='itemTwo'
-                src={require('../../Images/KaliLogo.png')}
+                src={require(projectsInfo[index].cardSRC)}
               />
             </a>
           </div>
           <div className='itemThird'>
-            <a href={projectsInfo[index + 2].cardhref} className="cardLinks">
-              <img
-                className='cardsItemImg'
-                alt='itemThree'
-                src={require('../../Images/KaliLogo.png')}
-              />
-            </a>
+            <img
+              className='cardsItemImg'
+              alt='itemThree'
+              src={require(projectsInfo[index].cardSRC)}
+            />
           </div>
         </div>
         {visibleTitle[projectsInfo[index + 1].itemTitle] &&
-          <div className='cardTitle' style={{ top: '40%', left: '40%', animation: 'moveleft 1.5s ease forwards 0.5s' }}>
+          <div className='cardTitle' style={{ top: '0%', animation: 'moveright 1s ease forwards 0.5s' }}>
             <p className='cardTitleText'>{projectsInfo[index + 1].cardTitleText}</p>
           </div>}
         {visibleTitle[projectsInfo[index + 1].itemTitle] &&
-          <div className='cardDate' style={{ top: '40%', left: '30%', animation: 'moveright 1s ease forwards' }}>
-            <p className='cardDateText'>{projectsInfo[index + 1].cardTitleDate}</p>
+          <div className='cardDate' style={{ top: '20%', animation: 'moveright 1s ease forwards' }}>
+            <p className='cardDateText'>{projectsInfo[index + 1].cardDateText}</p>
           </div>}
         <div className='nextArea' onClick={() => setIndex(index + 1)}>
-          <p className='nextText'>NEXT</p>
+          { (index != 4) && <i className='nextText'><HiOutlineArrowCircleDown/></i>}
         </div>
       </div>
     );
