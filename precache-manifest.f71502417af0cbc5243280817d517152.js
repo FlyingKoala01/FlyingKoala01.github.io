673f4292f85ba55ecdcfd0266744fb5a472b7d83
FlyingKoala01 <flyingkoala01@gmail.com>
2022-05-12 10:18:18 +0200

    Updates

---
+++ precache-manifest.f71502417af0cbc5243280817d517152.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "e28a4d3352af7482df04",
+    "url": "/static/css/main.fb1113fe.chunk.css"
+  },
+  {
+    "revision": "e28a4d3352af7482df04",
+    "url": "/static/js/main.f7ed32aa.chunk.js"
+  },
+  {
+    "revision": "b01891cb83a31ecd984a",
+    "url": "/static/js/runtime~main.e51718a9.js"
+  },
+  {
+    "revision": "8e11e2987d3cd206e720",
+    "url": "/static/js/2.6964ea15.chunk.js"
+  },
+  {
+    "revision": "c3162b243119d75204f9",
+    "url": "/static/js/3.c7726d44.chunk.js"
+  },
+  {
+    "revision": "c1742cf6953743a7175d98ee4b5e803f",
+    "url": "/static/media/KaliLogo.c1742cf6.png"
+  },
+  {
+    "revision": "1a15014557a0975199d9e5f5ca773bb1",
+    "url": "/static/media/reactProject.1a150145.png"
+  },
+  {
+    "revision": "8d5031a0e35e4bd5de44967ab12e2a0c",
+    "url": "/static/media/europe.8d5031a0.png"
+  },
+  {
+    "revision": "f23e1801922d79db3f8b44d46fec011c",
+    "url": "/index.html"
+  }
+];
