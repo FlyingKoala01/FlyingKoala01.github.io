effa04d0a2d5d588af7db16483d06a3b3d4f675c
FlyingKoala01 <isaac.iglesias.vila19@gmail.com>
2022-05-24 21:14:40 +0200

    Link bug

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,9 +10,9 @@ export default function Navbar() {
   useEffect(() => {
     const navs = [
       { name: "home", path: "/" },
-      { name: "about", path: "/AboutMe" },
-      { name: "portfolio", path: "/Portfolio" },
-      { name: "contact", path: "/ContactMe" }
+      { name: "about", path: "/about" },
+      { name: "portfolio", path: "/portfolio" },
+      { name: "contact", path: "/contact" }
     ];
     setNavLinks(navs);
   }, []);
